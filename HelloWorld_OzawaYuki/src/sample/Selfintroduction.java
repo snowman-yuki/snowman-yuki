275c8c88b57122568d34cd207d98c43cec1fe0b3
polar <polar@POTITA>
2025-08-13 15:27:57 +0900

    Java3導入

---
+++ HelloWorld_OzawaYuki/src/sample/Selfintroduction.java
@@ -0,0 +1,24 @@
+package sample;
+
+public class Selfintroduction {
+
+	public static void main(String[] args) {
+		//氏名
+		String name = "小澤 有希";
+		//年齢
+		int age = 27;
+		//趣味
+		String hobby = "音楽鑑賞";
+		
+		/*
+		 * 自己紹介文を出力する処理
+		 */
+		
+		System.out.println("私の名前は"+ name + "です" );
+		System.out.println("年齢は"+ age +"歳です");
+		System.out.println("趣味は"+ hobby +"です" );
+		
+
+	}
+
+}

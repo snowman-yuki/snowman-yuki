6cd03d5c691b261a7bf1633211d03e462bb39776
polar <polar@POTITA>
2025-08-14 17:28:18 +0900

    Java3導入

--- HelloWorld_OzawaYuki/src/sample/Selfintroduction.java
+++ HelloWorld_OzawaYuki/src/sample/Selfintroduction.java
@@ -17,7 +17,7 @@ public class Selfintroduction {
 		System.out.println("私の名前は"+ name + "です" );
 		System.out.println("年齢は"+ age +"歳です");
 		System.out.println("趣味は"+ hobby +"です" );
-		
+		System.out.println("何か好きな文字");
 
 	}
 

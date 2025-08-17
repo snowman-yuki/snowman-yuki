545470f7a67cdb0a8ef16c31984fd197fc810b14
polar <polar@POTITA>
2025-08-17 15:41:07 +0900

    Java7 変数①Qes1_1&2

---
+++ HelloWorld_OzawaYuki/src/curriculum_A/Qes1.java
@@ -0,0 +1,30 @@
+package curriculum_A;
+
+public class Qes1 {
+
+	public static void main(String[] args) {
+		byteNum // 変数byteNumの初期値を設定
+		shortNum // 変数shortの初期値を設定
+		intNum // 変数intNumの初期値を設定
+		longNum // 変数longNumの初期値を設定
+		floatNum // 変数floatNumの初期値を設定
+		doubleNum // 変数doubleNumの初期値を設定
+		char letter // 変数letterの初期値を設定
+		String letters // 変数lettersの初期値を設定
+		isBoolean // 変数isBooleanの初期値を設定
+		
+		
+		byteNum =10; // 変数byteNumに10を代入
+		shortNum = 100; // 変数shortNumに100を代入
+		intNum = 1000; // 変数intNumに1000を代入
+		longNum = 10000; // 変数longNumに10000を代入
+		floatNum = 9.5f; // 変数floatNumに9.5fを代入
+		doubleNum = 10.5; // 変数doubleNumに10.5を代入
+		char letter = 'a'; // 変数letterに'a'を代入
+		String letters = "ハロー"; // 変数lettersに"ハロー"を代入
+		isBoolean = true; // 変数isBooleanにtrueを代入
+		
+
+	}
+
+}
